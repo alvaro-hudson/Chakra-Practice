b75fbb5e4b9816dae6b4a5f4330ae8163ae06852
Alvaro <98783170+alvaro-hudson@users.noreply.github.com>
2022-08-22 16:46:17 +1200

    Add initial code for the details section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,16 @@ import {
   Container,
   Flex,
   VStack,
-  Heading
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Checkbox,
+  Button,
 } from '@chakra-ui/react';
 
 function App() {
@@ -14,6 +23,8 @@ function App() {
     <ChakraProvider theme={theme}>
       <Container maxWidth='container.xl' padding={0} /* p means padding*/>
         <Flex h='100vh' py={20} /* py means padding on y axis */>
+
+        {/* Details component */}
         <VStack 
           w="full"
           h='full'
@@ -21,8 +32,58 @@ function App() {
           spacing={10}
           alignItems='flex-start'
         >
-          <Heading size="2xl">Your details</Heading>
+          <VStack spacing={3} alignItems="flex-start">
+            <Heading size="2xl">Your details</Heading>
+            <Text>If you already have an account, click here to log in</Text>
+          </VStack>
+          <SimpleGrid columns={2} columnGap={3} rowGap={6} w='full'>
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>First Name</FormLabel>
+                <Input placeholder='John'></Input>
+              </FormControl>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>Last Name</FormLabel>
+                <Input placeholder='Doe'></Input>
+              </FormControl>
+            </GridItem>
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Address</FormLabel>
+                <Input placeholder='11 Generic Street'></Input>
+              </FormControl>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>City</FormLabel>
+                <Input placeholder='Wellington'></Input>
+              </FormControl>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>Country</FormLabel>
+                <Select>
+                  <option value="New Zealand">New Zealand</option>
+                  <option value="Australia">Australia</option>
+                  <option value="Austria">Austria</option>
+                  <option value="Chile">Chile</option>
+                  <option value="Germany">Germany</option>
+                </Select>
+              </FormControl>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Checkbox defaultChecked>Ship to billing</Checkbox>
+            </GridItem>
+            <GridItem colSpan={2}>
+              <Button size='lg' w='full'>Place Order</Button>
+            </GridItem>
+          </SimpleGrid>
+          
         </VStack>
+
+        {/* Cart component */}
         <VStack 
           w="full"
           h='full'
